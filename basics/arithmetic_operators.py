f8d11cc0bb4cf35302cd9e4f668eb99703bb84da
PRIYA <priyahin100@gmail.com>
2026-03-04 00:58:15 +0530

    Learnt Arithmetic Operators

---
+++ basics/arithmetic_operators.py
@@ -0,0 +1,10 @@
+# Topic : Arithmetic Operators
+a = 10
+b = 3
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  #Division 
+print(a // b)  # Floor Division
+print(a % b)  # Modulus
+print(a ** b)  # Exponentiation
