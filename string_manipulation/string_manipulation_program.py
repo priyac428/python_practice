a5a8d2b2bf840bdc8b4ddb361782662e9be1d75b
PRIYA <priyahin100@gmail.com>
2026-03-06 16:50:53 +0530

    Add string manipulation program

---
+++ string_manipulation/string_manipulation_program.py
@@ -0,0 +1,6 @@
+# Topic : String Manipulation Program
+msg=input("enter the message: ")
+print("uppercase",msg.upper())
+print("lowercase",msg.lower())
+print("relplaced",msg.replace(" ","_"))
+print("stripped",msg.strip())
