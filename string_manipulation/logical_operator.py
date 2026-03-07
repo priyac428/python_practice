4f910285e2e44d68965fe2125cdab5a3d9b8ad37
PRIYA <priyahin100@gmail.com>
2026-03-07 23:49:38 +0530

    Add logical operators

---
+++ string_manipulation/logical_operator.py
@@ -0,0 +1,21 @@
+# Topic : Logical Operator
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+
+if a > 10 and b > 10 :
+    print("yes")
+else:
+    print("no")
+
+if a < 5 or b < 5:
+    print("yes")
+else:
+    print("no")
+ 
+if not(a>b) :
+    print("yes")
+else:
+    print("no")
+ 
+
+
