1557460667eec53746631cffffbf39d98285be5f
PRIYA <priyahin100@gmail.com>
2026-03-10 22:09:16 +0530

    Add sort and reverse list

---
+++ list/sort_reverse.py
@@ -0,0 +1,8 @@
+# Topic : Reverse And Sort List
+numbers = [5, 2, 9, 1, 7]
+
+numbers.sort(reverse=True)
+print("Sorted in descending order:", numbers)
+
+numbers.reverse()
+print("Reversed list:", numbers)
