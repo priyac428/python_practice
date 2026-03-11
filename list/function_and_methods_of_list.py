89780edd48007376bf40cb2a514ff3969bd77e13
PRIYA <priyahin100@gmail.com>
2026-03-11 21:04:52 +0530

    Add list functions and methods

---
+++ list/function_and_methods_of_list.py
@@ -0,0 +1,14 @@
+# Topic :  Function And Methods Of List
+item = [1,5,7,4,6,1,4,6,3]
+
+print(len(item))                #list function
+print(sorted(item))
+print(sum(item))
+
+print(item.index(7))            #list methods
+print(item.count(4))
+item.reverse()
+print(item)
+item.sort()
+print(item)
+
