80296394ddaa392ecc3b80ab8b1515d177790b85
PRIYA <priyahin100@gmail.com>
2026-03-11 20:35:43 +0530

    Add list manipulation

---
+++ list/list_manipulation.py
@@ -0,0 +1,14 @@
+# Topic :  List Manipulation
+item = ["priya",200,30,40,50]
+item[1]=120
+print(item)
+item.append(0)
+print(item)
+item.insert(1,"CSE")
+print(item)
+item.remove(0)
+print(item)
+item.pop(2)
+print(item)
+item.clear()
+print(item)
