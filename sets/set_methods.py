eead29caf265b25e740cbc309e4591c2a1fbf498
PRIYA <priyahin100@gmail.com>
2026-03-13 20:27:47 +0530

    Add set methods

---
+++ sets/set_methods.py
@@ -0,0 +1,16 @@
+# Topic : Set Methods
+set = {"apple","mango","orange"}
+print(set)
+set.add("lemon")
+print(set)
+set.remove("lemon")
+print(set)
+set.discard("pineapple")
+print(set)
+set.pop()
+print(set)
+set.clear()
+print(set)
+
+
+
