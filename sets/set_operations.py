16a443e9a8c53bbb3d0395d1a23f9a5c3b42e443
PRIYA <priyahin100@gmail.com>
2026-03-13 20:12:09 +0530

    Add set operations

---
+++ sets/set_operations.py
@@ -0,0 +1,15 @@
+# Topic : Set Operations
+s1 = {1,2,3,5,8,6}
+s2 = {11,22,33,5,8,6}
+union = s1|s2
+print(union)
+intersection = s1&s2
+print(intersection)
+difference = s1-s2
+print(difference)
+set_diff =  s1^s2
+print(set_diff) 
+
+
+
+
