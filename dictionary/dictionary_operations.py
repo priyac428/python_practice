7e936375688905f3b867d0a4911cc9b269800142
PRIYA <priyahin100@gmail.com>
2026-03-14 22:28:12 +0530

    Add dictionary operations

---
+++ dictionary/dictionary_operations.py
@@ -0,0 +1,13 @@
+# Topic :  Dictionary operations
+a = {"bangalore" : "bisibelebath",
+     "mysore" : "mysore pak",
+     }
+print(a)
+a["davangere"] = "bennedose"
+print(a)
+a["bangalore"] = "pulav"
+print(a)
+del a["mysore"]
+print(a)
+print(a.keys())
+print(a.values())
