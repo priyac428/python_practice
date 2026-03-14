8725b43933f2c7623fe22eb784a3e5fbc4ad3e42
PRIYA <priyahin100@gmail.com>
2026-03-14 22:43:25 +0530

    Add  nested dictionary

---
+++ dictionary/nested_dictionary.py
@@ -0,0 +1,12 @@
+# Topic :  Nested Dictionary 
+friends = {
+    "Rahul": {
+        "favorite_subject": "Math",
+        "favorite_food": "Pizza"
+    },
+    "Anita": {
+        "favorite_subject": "Science",
+        "favorite_food": "Burger"
+    }
+}
+print(friends["Rahul"]["favorite_food"])
