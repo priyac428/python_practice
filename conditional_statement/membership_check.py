0649d1b2b86cc50bf4dfe08bb278b2ed60dbad56
PRIYA <priyahin100@gmail.com>
2026-03-15 22:34:14 +0530

    Add  membership checker

---
+++ conditional_statement/membership_check.py
@@ -0,0 +1,8 @@
+# Topic :  Membership Check
+age = int(input("enter your age: "))
+if age < 18:
+    print("you will get student membership")
+elif age >= 60:
+    print("you will get senior citizen membership")
+else:
+     print("you will get regular membership")
