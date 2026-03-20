cc9fc7daf6e178e42d5abde9f05db068e88090ad
PRIYA <priyahin100@gmail.com>
2026-03-20 19:56:12 +0530

    Add odd number printer

---
+++ loops/odd_number_printer.py
@@ -0,0 +1,13 @@
+# Topic : Odd Number Printer
+i = 0
+while i < 20:
+    i += 1
+    if i % 2 ==0:
+        continue
+    print(i)
+
+i = 1
+while i <= 20:
+    if i % 2 != 0:
+        print(i)
+    i = i + 1
