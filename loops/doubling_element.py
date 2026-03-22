2874756cc298c963a172678f0a3c81ab72c4e52b
PRIYA <priyahin100@gmail.com>
2026-03-22 21:31:42 +0530

    Add doubling element

---
+++ loops/doubling_element.py
@@ -0,0 +1,12 @@
+# Topic : Doubling Element
+l = [1,2,3,4]
+dl = []
+for num in l:
+    dl.append(num*2)
+print(dl)
+
+#oR
+double = [num*2 for num in l]     #using list Comprehension
+print(double)
+
+
