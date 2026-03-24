482dfcbc694b604d746643346eb9b999c04e9c1d
PRIYA <priyahin100@gmail.com>
2026-03-24 23:21:32 +0530

    Add student information

---
+++ loops/student_information.py
@@ -0,0 +1,11 @@
+# Topic : Student Information
+students = [
+    {"name": "Priya", "age": 20, "marks": 85},
+    {"name": "Rahul", "age": 21, "marks": 90},
+    {"name": "Anu", "age": 19, "marks": 88}
+]
+for student in students:
+    print("Name:", student["name"])
+    print("Age:", student["age"])
+    print("Marks:", student["marks"])
+    print("-------------------")
