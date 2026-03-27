413cd6ad5f9fb0d9ed42e8266d2a847966581624
PRIYA <priyahin100@gmail.com>
2026-03-27 20:53:16 +0530

    Add parameters and arguments

---
+++ functions/parameters_arguments.py
@@ -0,0 +1,11 @@
+# Topic : Parameters and Arguments
+def marriage(boy,girl):
+    print(f"{boy} marries {girl}")
+
+marriage("ayush","amaya")  #positional arguments
+marriage(boy="sushanth",girl="samaya") #keyword arguments
+
+def names(name="none"):#default parameter
+    print(name)
+
+names()
