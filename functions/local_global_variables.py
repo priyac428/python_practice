3e4d11bac0d776c7e155d05f916c3a75ea52fcb5
PRIYA <priyahin100@gmail.com>
2026-03-27 22:32:26 +0530

    Add local and global variables

---
+++ functions/local_global_variables.py
@@ -0,0 +1,9 @@
+# Topic : Local And Global Variables
+x = "priya" # global variable
+def name():
+    y = "keerthi" # local variable
+    print(y)
+    print(x)
+name()
+print(x)
+# print(y) error as y is local variable
