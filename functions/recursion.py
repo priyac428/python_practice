7da43a60980aad50f427960c78f33bdba47a476e
PRIYA <priyahin100@gmail.com>
2026-03-28 20:53:26 +0530

    Add recursion

---
+++ functions/recursion.py
@@ -0,0 +1,14 @@
+# Topic : Recursion
+def fact(n):
+    if n == 1:
+        return 1
+    else :
+        return n * fact(n-1)
+print(fact(5))
+
+def sum(n):
+    if n == 1:
+        return 1
+    else :
+        return n + sum(n-1)
+print(sum(5))
