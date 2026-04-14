93b127cfa1cef045233cb2b5b5925d4e72b49b00
PRIYA <priyahin100@gmail.com>
2026-04-14 22:51:48 +0530

    Add updated

--- functions/greet_example.py
+++ functions/greet_example.py
@@ -7,3 +7,9 @@ greet()
 def greet_user(name):
     print(f"Hello {name}!")
 greet_user("priya")
+
+def greet1(name="Friend"):
+    print("Hello", name)
+
+greet1()          # default
+greet1("Priya")   # custom
