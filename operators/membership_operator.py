08045dddde0cadeeb7ede842ca4e8d82c4e94fec
PRIYA <priyahin100@gmail.com>
2026-04-21 23:00:46 +0530

    Add updated

--- operators/membership_operator.py
+++ operators/membership_operator.py
@@ -2,3 +2,23 @@
 a=input("enter your message: ")
 print("a"in a)
 print("python" not in a)
+
+# list of registered students
+students = ["Rahul", "Priya", "Amit", "Sneha"]
+
+# user trying to login
+name = input("Enter your name: ")
+
+# check membership
+if name in students:
+    print("Login Successful")
+else:
+    print("Access Denied")
+
+# checking not in
+blocked_users = ["Amit"]
+
+if name not in blocked_users:
+    print("You are allowed to continue")
+else:
+    print("You are blocked")
