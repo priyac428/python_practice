b87e0791e5efbad337c899f8af4292343375d2ef
PRIYA <priyahin100@gmail.com>
2026-07-24 18:49:37 +0530

    updated

--- conditional_statement/match_case.py
+++ conditional_statement/match_case.py
@@ -12,6 +12,29 @@ match operator :
     case "/" :
         print(f"{a/b}")
 
+def whichWeekDay(day):
+    match day:
+        case 1:
+            print("Monday")
+        case 2:
+            print("Tuesday")
+        case 3:
+            print("Wednesday")
+        case 4:
+            print("Thursday")
+        case 5:
+            print("Friday")
+        case 6:
+            print("Saturday")
+        case 7:
+            print("Sunday")
+        case _ :
+            print("Invalid")
+whichWeekDay(8)
+
+
+                
+
 
 
 
